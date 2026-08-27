bd789ba99a8320c05a845eec85090ccf03ed0c09
Alireza Vafaei Sadr <vafaei.sadr@gmail.com>
2026-08-27 18:09:17 -0400

    Point reviewer reanalysis runner to corrected v2

--- reviewer_reanalysis/run_reviewer_reanalysis.py
+++ reviewer_reanalysis/run_reviewer_reanalysis.py
@@ -1,1 +1,6 @@
-PLACEHOLDER
+#!/usr/bin/env python3
+"""Compatibility wrapper for the corrected reviewer reanalysis."""
+from run_reviewer_reanalysis_v2 import main
+
+if __name__ == '__main__':
+    main()
